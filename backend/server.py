8ca6f08bec49701bb200b8ce9bd0dfd8f6eb4fa6
ravi5790 <devilkingravi43521@gmail.com>
2026-04-27 17:05:46 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -585,3 +585,10 @@ async def on_startup():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
